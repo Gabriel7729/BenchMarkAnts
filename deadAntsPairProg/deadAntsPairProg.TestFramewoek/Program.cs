d07427e6b8376bcc3efc1e4d86753168e58ade53
Pedro Javier Lora <javierpd45@gmail.com>
2024-03-07 15:05:52 -0400

    Tests with xUnit

--- deadAntsPairProg/deadAntsPairProg.TestFramewoek/Program.cs
+++ deadAntsPairProg/deadAntsPairProg.TestFramewoek/Program.cs
@@ -1,57 +1,57 @@
-﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
-using System.Diagnostics;
+﻿//using Microsoft.VisualStudio.TestPlatform.TestHost;
+//using System.Diagnostics;
 
-namespace AntsV2.Test
-{
-    public class DeadAntsTest
-    {
-        [Fact]
-        public void ComparePerformance()
-        {
-            string testString = "...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..an";
+//namespace AntsV2.Test
+//{
+//    public class DeadAntsTest
+//    {
+//        [Fact]
+//        public void ComparePerformance()
+//        {
+//            string testString = "...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..an";
 
-            var stopwatch = Stopwatch.StartNew();
-            Program.DeadCounterAnts(testString);
-            stopwatch.Stop();
-            var timeForDeadCounterAnts = stopwatch.Elapsed.TotalMilliseconds;
+//            var stopwatch = Stopwatch.StartNew();
+//            Program.DeadCounterAnts(testString);
+//            stopwatch.Stop();
+//            var timeForDeadCounterAnts = stopwatch.Elapsed.TotalMilliseconds;
 
-            stopwatch.Restart();
-            Program.DeadCounterAntsV2(testString);
-            stopwatch.Stop();
-            var timeForDeadCounterAntsV2 = stopwatch.Elapsed.TotalMilliseconds;
+//            stopwatch.Restart();
+//            Program.DeadCounterAntsV2(testString);
+//            stopwatch.Stop();
+//            var timeForDeadCounterAntsV2 = stopwatch.Elapsed.TotalMilliseconds;
 
-            Assert.True(timeForDeadCounterAntsV2 < timeForDeadCounterAnts);
-        }
+//            Assert.True(timeForDeadCounterAntsV2 < timeForDeadCounterAnts);
+//        }
 
-        [Fact]
-        public void ShouldValidateAllAntsAlive()
-        {
-            Assert.Equal(0, Program.DeadCounterAntsV2("ant ant ant"));
-        }
+//        [Fact]
+//        public void ShouldValidateAllAntsAlive()
+//        {
+//            Assert.Equal(0, Program.DeadCounterAntsV2("ant ant ant"));
+//        }
 
-        [Fact]
-        public void ShouldValidateAllAntsAreDead()
-        {
-            Assert.Equal(2, Program.DeadCounterAntsV2("atn n"));
-        }
+//        [Fact]
+//        public void ShouldValidateAllAntsAreDead()
+//        {
+//            Assert.Equal(2, Program.DeadCounterAntsV2("atn n"));
+//        }
 
-        [Fact]
-        public void ShouldValidateMoreHeads()
-        {
-            Assert.Equal(4, Program.DeadCounterAnts("a a a a n t t t t"));
-        }
+//        [Fact]
+//        public void ShouldValidateMoreHeads()
+//        {
+//            Assert.Equal(4, Program.DeadCounterAnts("a a a a n t t t t"));
+//        }
 
-        [Fact]
-        public void ShouldValidateMoreTails()
-        {
-            Assert.Equal(5, Program.DeadCounterAntsV2("t t t t t a n"));
-        }
+//        [Fact]
+//        public void ShouldValidateMoreTails()
+//        {
+//            Assert.Equal(5, Program.DeadCounterAntsV2("t t t t t a n"));
+//        }
 
-        [Fact]
-        public void ShouldValidateIfStringOrNull()
-        {
-            Assert.Equal(0, Program.DeadCounterAntsV2(""));
-            Assert.Equal(0, Program.DeadCounterAntsV2(null));
-        }
-    }
-}
+//        [Fact]
+//        public void ShouldValidateIfStringOrNull()
+//        {
+//            Assert.Equal(0, Program.DeadCounterAntsV2(""));
+//            Assert.Equal(0, Program.DeadCounterAntsV2(null));
+//        }
+//    }
+//}
---
+++ deadAntsPairProg/deadAntsTestxUnit/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deadAntsTestxUnit
+{
+    public class Program
+    {
+        public static int DeadCounterAntsV2(string ants)
+        {
+            if (string.IsNullOrEmpty(ants))
+                return 0;
+
+            int heads = 0;
+            int bodies = 0;
+            int tails = 0;
+            int deadAnts;
+
+            for (int i = 0; i < ants.Length; i++)
+            {
+                if (ants[i] == 'a') heads++;
+                else if (ants[i] == 'n') bodies++;
+                else if (ants[i] == 't') tails++;
+
+                if (i >= 2 && ants[i - 2] == 'a' && ants[i - 1] == 'n' && ants[i] == 't')
+                {
+                    heads--;
+                    bodies--;
+                    tails--;
+                    continue;
+                }
+
+                //if (ants[i] == 'a') heads++;
+                //else if (ants[i] == 'n') bodies++;
+                //else if (ants[i] == 't') tails++;
+            }
+
+            deadAnts = Math.Max(heads, bodies);
+            deadAnts = Math.Max(deadAnts, tails);
+
+            return deadAnts;
+        }
+
+        public static int DeadCounterAnts(string antsTrial)
+        {
+            if (string.IsNullOrEmpty(antsTrial))
+                return 0;
+
+            string replaceAnts = antsTrial.Replace("ant", string.Empty);
+
+            int aCount = replaceAnts.Count(x => x == 'a');
+            int nCount = replaceAnts.Count(x => x == 'n');
+            int tCount = replaceAnts.Count(x => x == 't');
+
+            return Math.Max(aCount, Math.Max(nCount, tCount));
+        }
+    }
+}
